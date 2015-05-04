2167d532f430e622aa35e333dbbae71f81a22c58
Adrien Nhem <adrien.nhem@gmail.com>
2015-05-04 11:48:54 +0200

    improved about us

--- config/routes.rb
+++ config/routes.rb
@@ -20,8 +20,8 @@ Rails.application.routes.draw do
     resources :lessons
   end
 
-
   root 'pages#home'
+  # root 'pages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
