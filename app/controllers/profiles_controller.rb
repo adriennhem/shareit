1766480bad3206fdaca4ca0c8487ae76ddcf0d17
adriennhem <adrien.nhem@gmail.com>
2015-08-27 22:54:59 +0200

    tracking works

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,6 +3,7 @@ before_action :authenticate_user!
 
 def show
 	@user = current_user
+	
 end
 
 end
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,4 +2,6 @@ class Lesson < ActiveRecord::Base
 	has_one :lecture, through: :chapter
 	belongs_to :chapter 
 	has_many :user_lessons
+	has_many :users, through: :user_lessons
+
 end
