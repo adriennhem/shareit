c3b4c3bd5d2529820fe974c549b7e0fe97e86281
adriennhem <adrien.nhem@gmail.com>
2015-10-26 20:38:11 +0000

    fuck this chnage in role

--- db/migrate/20150925191735_change_role_in_users.rb
+++ db/migrate/20150925191735_change_role_in_users.rb
@@ -1,5 +1,5 @@
 class ChangeRoleInUsers < ActiveRecord::Migration
   def change
-  	change_column :users, :role, 'integer USING CAST("role" AS integer)
+  	change_column :users, :role, 'integer USING CAST("role" AS integer)'
   end
 end
