c9443b274159584b8d8e6c65f89e6207ece192ae
adriennhem <adrien.nhem@gmail.com>
2016-01-23 16:54:53 +0000

    updated

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
 	address: "smtp.mandrillapp.com",
 	port: 587,
 	enables_starttls_auto: true,
-	user_name: ENV["MANDRILL_NAME"],
-	password: ENV["MANDRILL_PASSWORD"],
+	user_name: ENV['MANDRILL_NAME'],
+	password: ENV['MANDRILL_PASSWORD'],
 	authentification: "login"
 
 }
