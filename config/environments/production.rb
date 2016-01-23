858d33453988065b0afc3d5ffb88a41271246dd6
adriennhem <adrien.nhem@gmail.com>
2016-01-23 18:00:43 +0000

    fixed image users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ config.action_mailer.raise_delivery_errors = false
   :storage => :s3,
   :s3_host_name => 's3-us-west-2.amazonaws.com',
   :s3_credentials => {
-    :bucket => 'ENV["S3_BUCKET_NAME"]',
+    :bucket => ENV["S3_BUCKET_NAME"],
     :access_key_id => ENV["AWS_ACCESS_KEY_ID"],
     :secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"]
   }
