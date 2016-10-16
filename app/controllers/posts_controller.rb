fcfb9e3dd058cd90242714d3c583d3c3ba5e39b3
adriennhem <adrien.nhem@gmail.com>
2016-10-16 19:01:03 +0100

    added blog posts recommendations

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,6 +17,7 @@ class PostsController < ApplicationController
 
 	def show
 		authorize @post
+		@recommended_posts = Post.where(blog_category_id: @post.blog_category_id).last(3)
 	end
 
 	def feed
